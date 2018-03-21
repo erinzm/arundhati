7039038978d2939b58e70ca5c5ba712dba57818d
Liam <liam@cpan.org>
2018-03-21 13:22:44 -0500

    prepare_20groups.py: output tfidf name mapping

--- data/prepare_20groups.py
+++ data/prepare_20groups.py
@@ -10,5 +10,7 @@ vectorizer = TfidfVectorizer()
 vectors = vectorizer.fit_transform(newsgroups.data)
 
 save_npz('20groups-tfidf.npz', vectors)
+with open('20groups-tfidf-names.json', 'w') as f:
+    json.dump(vectorizer.get_feature_names(), f)
 with open('20groups.json', 'w') as f:
     json.dump(newsgroups.data, f)
