15ebad396c60f8e0a5fe0222c77e7dbd8684d3e2
Liam <liam@cpan.org>
2018-03-21 13:07:35 -0500

    add script to pull 20groups data and do tfidf

---
+++ data/prepare_20groups.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import save_npz
+import json
+
+newsgroups = fetch_20newsgroups(subset='all',
+                                remove=['headers', 'footers', 'quotes'])
+
+vectorizer = TfidfVectorizer()
+vectors = vectorizer.fit_transform(newsgroups.data)
+
+save_npz('20groups-tfidf.npz', vectors)
+with open('20groups.json', 'w') as f:
+    json.dump(newsgroups.data, f)
