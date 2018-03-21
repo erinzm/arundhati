85f2beffbdee54680b4be747e9cf5eb660db3e10
Liam <liam@cpan.org>
2018-03-21 13:23:43 -0500

    move prepare_20groups.py to scripts/

--- scripts/prepare_20groups.py
+++ scripts/prepare_20groups.py
@@ -9,8 +9,8 @@ newsgroups = fetch_20newsgroups(subset='all',
 vectorizer = TfidfVectorizer()
 vectors = vectorizer.fit_transform(newsgroups.data)
 
-save_npz('20groups-tfidf.npz', vectors)
-with open('20groups-tfidf-names.json', 'w') as f:
+save_npz('data/20groups-tfidf.npz', vectors)
+with open('data/20groups-tfidf-names.json', 'w') as f:
     json.dump(vectorizer.get_feature_names(), f)
-with open('20groups.json', 'w') as f:
+with open('data/20groups.json', 'w') as f:
     json.dump(newsgroups.data, f)
